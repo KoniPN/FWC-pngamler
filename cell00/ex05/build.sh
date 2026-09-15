cc2e5a999b6a79b45595ac250a916c9808ceee37
KoniPN <kong.18th@gmail.com>
2026-09-15 16:01:59 +0700

    edit ex05

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-if [ -z "$var" ]
-then
-    echo "No arguments supplied."
-    exit 1
-fi
-
 for var in "$@"
 do
+
+    if [ -z "$var" ]
+    then
+        echo "No arguments supplied."
+        exit 1
+    fi
     mkdir ex$var
 done
